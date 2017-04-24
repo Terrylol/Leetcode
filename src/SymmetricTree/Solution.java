eac45b93f99dfd1b27b42558471e2f14f9dd7b1f
Terrylol <952329690@qq.com>
2017-04-24 10:47:56 +0800

    Git Tree

---
+++ src/SymmetricTree/Solution.java
@@ -0,0 +1,53 @@
+package SymmetricTree;
+
+        import TreeNode.TreeNode;
+
+        import java.util.Stack;
+
+/**
+ * Created by 95232 on 2017/4/24.
+ */
+public class Solution {
+    public boolean isSym(TreeNode root){
+        return root==null || isSym(root.getLeft(),root.getRight());
+    }
+
+    public boolean isSym(TreeNode left,TreeNode right){
+        if(left==null || right==null)
+            return left == right;
+        if(left.getVal()==right.getVal())
+            return false;
+        return isSym(left.getLeft(),right.getRight())&&isSym(left.getRight(),right.getLeft());
+    }
+
+    public boolean isstack(TreeNode root){
+        if(root == null) return true;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode left,right;
+        if(root.getLeft()!=null){
+            if(root.getRight()==null)return false;
+            stack.push(root.getLeft());
+            stack.push(root.getRight());
+        }
+        else if(root.getRight()!=null) return false;
+        while (!stack.isEmpty()){
+            if(stack.size()%2!=0)return false;
+            right=stack.pop();
+            left=stack.pop();
+            if(left.getLeft()!=null){
+                if(right.right==null)   return false;
+                stack.push(left.left);
+                stack.push(right.right);
+            }
+            else if(right.right!=null){
+                return false;
+            }
+
+            if(left.right!=null){
+                if(right.left==null)   return false;
+                stack.push(left.right);
+                stack.push(right.left);
+            }
+            else if(right.left!=null) return false;
+        }
+    }}
---
+++ src/TreeNode/TreeNode.java
@@ -0,0 +1,35 @@
+package TreeNode;
+
+/**
+ * Created by 95232 on 2017/4/24.
+ */
+public class TreeNode {
+    int val;
+    public TreeNode left;
+    public TreeNode right;
+    TreeNode(int x) { val = x; }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+}
