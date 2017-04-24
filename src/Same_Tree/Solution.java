3b91e28c34e8a35480df1eb5d474ca5ce9621b3f
Terrylol <952329690@qq.com>
2017-04-24 10:32:53 +0800

    Git Tree

---
+++ src/Same_Tree/Solution.java
@@ -0,0 +1,17 @@
+package Same_Tree;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * Created by 95232 on 2017/4/24.
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q){
+        if(p==null && q==null) return true;
+        if(p==null ||q== null) return false;
+        if(p == q){
+             //diedai
+        }
+        return false;
+    }
+}
